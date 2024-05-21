764d89e2967c6227f1d10fb15d18dfea32ff96a4
Bagration <bagration-34@yandex.ru>
2024-05-21 17:51:13 +0300

    Создал проект

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<script setup>
+  import MyCanvas from './components/MyCanvas.vue';
+</script>
+
+<template>
+  <div class="app">
+    <h1 class="h1">TANKS 2D</h1>
+    <MyCanvas/>
+  </div>
+</template>
+
+<style scoped>
+  .app{
+    font-family: Helvetica, sans-serif;
+    text-align: center;
+    margin-top: 40px;
+  }
+</style>
---
+++ src/components/MyCanvas.vue
@@ -0,0 +1,13 @@
+<template>
+  <canvas id="myCanvas"></canvas>
+</template>
+
+<script setup></script>
+
+<style scoped>
+  #myCanvas {
+    height: 512px;
+    width: 512px;
+    background-color: rgb(167, 167, 167);
+  }
+</style>
